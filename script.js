9d81226ab5c85b195f3c7a1336e61918349811d5
loI-1 <phamhuuloi0210@gmail.com>
2025-04-30 20:22:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<script>
 const urlParams = new URLSearchParams(window.location.search);
 const examId = urlParams.get('examId');
 fetch('data/exams.json')
@@ -39,4 +38,3 @@ fetch('data/exams.json')
       document.getElementById('result').innerHTML = `<h2>Score: ${score} / ${exam.questions.length}</h2>`;
     }
   });
-</script>
